7bd08cf4d8642b47c4464c612e821b13d85a2a3d
Gustavo de Oliveira <guxtavodeoliveira@gmail.com>
2025-06-28 10:57:09 -0300

    Implementa ajustes no Program.cs e Reserva.cs

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("Quantidade de hospedes a mais do suportado");
             }
         }
 
@@ -37,24 +38,23 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = valor * 10 / 100;
+                valor = valor - desconto;
+                return valor;
             }
-
-            return valor;
+            else
+            {
+                return valor;
+            }
+           
         }
     }
 }
